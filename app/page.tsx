4f1b54c15f5156a8716aa518962a5fffcce2805c
Adilson Júnior <adilsonguimaraes1@hotmail.com>
2026-04-24 15:05:53 -0300

    commit inicial

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return(
+
+<main>
+  <h1>Hello, world!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga hic officiis vel sed, repellendus eligendi quaerat tempore soluta quia quidem nihil assumenda dicta delectus architecto commodi? Ullam beatae nesciunt veniam.</p>
+</main>
+)
+}
